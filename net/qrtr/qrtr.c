9c04ba2374ae1746234f504c0ca0d1bee4749e57
Pavel Skripkin <paskripkin@gmail.com>
2021-07-23 18:31:32 +0300

    net: qrtr: fix memory leaks
    [ Upstream commit 52f3456a96c06760b9bfae460e39596fec7af22e ]
    
    Syzbot reported memory leak in qrtr. The problem was in unputted
    struct sock. qrtr_local_enqueue() function calls qrtr_port_lookup()
    which takes sock reference if port was found. Then there is the following
    check:
    
    if (!ipc || &ipc->sk == skb->sk) {
    	...
    	return -ENODEV;
    }
    
    Since we should drop the reference before returning from this function and
    ipc can be non-NULL inside this if, we should add qrtr_port_put() inside
    this if.
    
    The similar corner case is in qrtr_endpoint_post() as Manivannan
    reported. In case of sock_queue_rcv_skb() failure we need to put
    port reference to avoid leaking struct sock pointer.
    
    Fixes: e04df98adf7d ("net: qrtr: Remove receive worker")
    Fixes: bdabad3e363d ("net: Add Qualcomm IPC router")
    Reported-and-tested-by: syzbot+35a511c72ea7356cdcf3@syzkaller.appspotmail.com
    Signed-off-by: Pavel Skripkin <paskripkin@gmail.com>
    Reviewed-by: Manivannan Sadhasivam <mani@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- net/qrtr/qrtr.c
+++ net/qrtr/qrtr.c
@@ -956,8 +956,10 @@ int qrtr_endpoint_post(struct qrtr_endpoint *ep, const void *data, size_t len)
 			}
 		}
 
-		if (sock_queue_rcv_skb(&ipc->sk, skb))
+		if (sock_queue_rcv_skb(&ipc->sk, skb)) {
+			qrtr_port_put(ipc);
 			goto err;
+		}
 
 		/**
 		 * Force wakeup for all packets except for sensors and blacklisted services
@@ -1594,6 +1596,8 @@ static int qrtr_local_enqueue(struct qrtr_node *node, struct sk_buff *skb,
 		return 0;
 	}
 	if (!ipc || &ipc->sk == skb->sk) { /* do not send to self */
+		if (ipc)
+			qrtr_port_put(ipc);
 		kfree_skb(skb);
 		return -ENODEV;
 	}
