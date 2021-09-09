f9e595fc4aa1ecdb42538c4fa5aa0e758a40cd6e
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-10 00:47:53 +0300

    techpack: display: track real backlight value

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -665,6 +665,8 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 		bl->bl_level_nodim = bl_lvl;
 	}
 
+	bl->real_bl_level = bl_lvl;
+
 	return rc;
 }
 
@@ -2428,6 +2430,7 @@ static int dsi_panel_parse_bl_config(struct dsi_panel *panel)
 
 	panel->bl_config.bl_scale = MAX_BL_SCALE_LEVEL;
 	panel->bl_config.bl_scale_sv = MAX_SV_BL_SCALE_LEVEL;
+	panel->bl_config.real_bl_level = 0;
 
 	rc = utils->read_u32(utils->data, "qcom,mdss-dsi-bl-min-level", &val);
 	if (rc) {
