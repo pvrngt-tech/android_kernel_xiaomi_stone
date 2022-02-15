77a1e10e4f36eea3a0f3cf5002c408a06f0e6eee
Gustavo A. R. Silva <gustavoars@kernel.org>
2022-02-14 19:11:44 -0600

    treewide: Replace zero-length arrays with flexible-array members
    There is a regular need in the kernel to provide a way to declare
    having a dynamically sized set of trailing elements in a structure.
    Kernel code should always use “flexible array members”[1] for these
    cases. The older style of one-element or zero-length arrays should
    no longer be used[2].
    
    This code was transformed with the help of Coccinelle:
    (next-20220214$ spatch --jobs $(getconf _NPROCESSORS_ONLN) --sp-file script.cocci --include-headers --dir . > output.patch)
    
    @@
    identifier S, member, array;
    type T1, T2;
    @@
    
    struct S {
      ...
      T1 member;
      T2 array[
    - 0
      ];
    };
    
    UAPI and wireless changes were intentionally excluded from this patch
    and will be sent out separately.
    
    [1] https://en.wikipedia.org/wiki/Flexible_array_member
    [2] https://www.kernel.org/doc/html/v5.16/process/deprecated.html#zero-length-and-one-element-arrays
    
    Link: https://github.com/KSPP/linux/issues/78
    Reviewed-by: Kees Cook <keescook@chromium.org>
    [Tashar02: Backport to msm-5.4 by manually running the coccinelle script]
    Co-authored-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- techpack/audio/include/dsp/apr_audio-v2.h
+++ techpack/audio/include/dsp/apr_audio-v2.h
@@ -2433,7 +2433,7 @@ struct afe_port_cmd_event_cfg {
 	 * Each event has the structure of
 	 * afe_port_cmd_mod_evt_cfg_payload.
 	 */
-	uint8_t payload[0];
+	uint8_t payload[];
 };
 
 /** Event registration for a module. */
